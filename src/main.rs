8a885958c35516bf2b91698d61d0a2a4855f6dd1
Sugar-Coder <13169670256@163.com>
2023-02-08 16:05:56 +0800

    set crate name egui_plotter

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "egui plotter",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(egui_plotter::TemplateApp::new(cc))),
     );
 }
 
@@ -30,7 +30,7 @@ fn main() {
         eframe::start_web(
             "the_canvas_id", // hardcode it
             web_options,
-            Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+            Box::new(|cc| Box::new(egui_plotter::TemplateApp::new(cc))),
         )
         .await
         .expect("failed to start eframe");
